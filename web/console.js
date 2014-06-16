e333a193eb9de6b27150b84bbf962ff946c0cafd
Beau Collins <beaucollins@gmail.com>
2014-06-16 12:15:54 -0700

    Tooltip positioning

--- web/console.js
+++ web/console.js
@@ -896,7 +896,7 @@ ParamBuilder.prototype.setupFieldInput = function(name, node) {
         var value = node.html().replace(/<br><br>$/, '\n').replace(/<br>/g, '\n');
         builder.setParam(name, value);
       },
-      tip = $.extend({name: name, context:node}, this.value[name]);
+      tip = $.extend({name:name, context:node}, this.value[name]);
   return this.setupInput(node, format).on('focus', fn.arglock(this.focusListener, tip));
 };
 
@@ -1057,7 +1057,12 @@ var PathField = Component.extend({
 
     this.clearNodes();
 
-    this.node.addClass('endpoint');
+    if (this.hasSelection()) {
+     this.node.addClass('endpoint');
+    } else {
+      this.node.removeClass('endpoint');
+    }
+
     this.focusable_parts = [];
     path.parts.forEach(buildNode.bind(this));
 
@@ -1070,9 +1075,6 @@ var PathField = Component.extend({
     }
 
     this.last_query = "";
-  },
-  'onReset' : function() {
-    this.node.removeClass('endpoint');
   }
 });
 
@@ -1283,53 +1285,149 @@ var Component = require('./component'),
     fn = require('../fn');
 
 var Tip = Component.extend({
+  'opacity' : '0.9',
   'init' : function() {
     this.header = this.node.find('header');
     this.name = this.header.find('code');
     this.type = this.header.find('em');
     this.anchor = this.node.find('.anchor');
     this.cancel = function() {};
+    this.contextBlurListener = this.hide.bind(this);
 
     this.positionDelayed = fn.rateLimit(200, this.positionTip.bind(this));
 
     this.hide();
   },
   'onChange' : function(value, oldValue) {
+
+    if (this.currentContext) {
+      this.currentContext.off('blur', this.contextBlurListener);
+    }
+
+    if (!value) {
+      return;
+    }
+
     this.name.text(value.name);
     this.type.text(value.type);
+
 
     this.displayDescription(value.description);
 
     if (value.context) {
+      this.currentContext = value.context;
       this.cancel();
       this.cancel = this.positionDelayed(value.context, value.position);
     }
   }
 });
 
-Tip.prototype.positionTip = function(context, position) {
+Tip.prototype.positionTip = function(context, position, attempts) {
 
   var offset = context.offset(),
+      $body = $('body'),
+      screen,
+      bounds,
       positionCss = {},
-      anchorCss = {};
+      anchorCss = {},
+      retry = false;
 
-  this.node.css({'opacity':'0', 'visibility': 'hidden'}).show();
+  attempts = attempts || [];
+  position = position || 'right';
 
-  if (position === 'below') {
+  this.node.hide();
+
+  limit = {
+    width: $body.width(),
+    height: $body.height()
+  };
+
+  // layout to get width/height
+  this.node.css({'opacity':'0', 'visibility': 'hidden', left:0, top:0, 'width': null}).show();
+
+  // store the bounds
+  bounds = this.node.offset();
+
+  this.node.css({'width': bounds.width});
+
+  // determine where it should go relative the context
+  switch (position) {
+  case 'above':
+    positionCss = {top: offset.top - bounds.height - 8, left: offset.left };
+    anchorCss = {top: bounds.height, left: 16};
+    break;
+  case 'below':
     positionCss = {top: offset.top + offset.height + 8, left: offset.left};
     anchorCss = { top: 0, left: 16};
-  } else {
+    break;
+  case 'right':
     positionCss = {top: offset.top, left: offset.width + offset.left + 8};
     anchorCss = { top: 16, left: 0};
+    break;
+  case 'left':
+    positionCss = {top: offset.top, left: offset.left - bounds.width - 8};
+    anchorCss = { top: 16, left: bounds.width };
+    break;
   }
-
-  positionCss.visibility = 'visible';
-  positionCss.opacity = 0.9;
 
   this.anchor.css(anchorCss);
   this.node.css(positionCss);
 
-  context.one('blur', this.hide.bind(this));
+  bounds = this.node.offset();
+  bounds.right = bounds.left + bounds.width;
+  bounds.bottom = bounds.top + bounds.height;
+
+  switch (position) {
+  case 'below':
+    if (bounds.bottom > limit.height) {
+      retry = 'right';
+    }
+    break;
+  case 'above':
+    if (bounds.top < 0) {
+      retry = 'left';
+    }
+    break;
+  case 'left':
+    if (bounds.left < 0) {
+      retry = 'below';
+    }
+    break;
+  case 'right':
+    if (bounds.right > limit.width) {
+      retry = 'above';
+    }
+    break;
+  }
+
+  if (retry !== false) {
+    if (attempts.indexOf(retry) === -1) {
+      this.positionTip(context, retry, attempts.concat(position));
+      return;
+    }
+  }
+
+  var adjust, delta;
+
+  if (bounds.right > limit.width - 8) {
+    adjust = limit.width - bounds.width - 8;
+    delta = adjust - bounds.left;
+
+    this.node.css('left', adjust);
+    this.anchor.css({'left': anchorCss.left - delta});
+  }
+
+  if (bounds.bottom > limit.height - 8) {
+    adjust = Math.max(8,limit.height - bounds.height - 8);
+    delta = adjust - bounds.top;
+
+    this.node.css('top', adjust);
+    this.anchor.css('top', anchorCss.top - delta);
+  }
+
+  this.node.css({'visibility':'visible', 'opacity':this.options.opacity});
+
+  context.one('blur', this.contextBlurListener);
 
 };
 
@@ -1354,7 +1452,7 @@ Tip.prototype.displayDescription = function(description) {
 
 Tip.prototype.formatDescription = function(name, description) {
 
-  var item = $('<li></li>'), def = '(default)';
+  var item = $('<li></li>'), def = '(default)', opt = 'Optional.';
 
   if (!description) {
     description = name;
@@ -1367,8 +1465,16 @@ Tip.prototype.formatDescription = function(name, description) {
   }
 
   if (description.indexOf(def) === 0) {
-    item.append($('<em></em>').text('default')).append(' ');
+    item.append($('<em></em>').text('Default'));
     description = description.slice(def.length);
+    if (description.trim() !== '') {
+      item.append(' – ');
+    }
+  }
+
+  if (description.indexOf(opt) === 0) {
+    item.append($('<em></em>').text('Optional')).append(' – ');
+    description = description.slice(opt.length);
   }
 
   return item.append($('<span></span>').text(description));
@@ -1376,6 +1482,7 @@ Tip.prototype.formatDescription = function(name, description) {
 };
 
 Tip.prototype.hide = function() {
+  this.value = null;
   this.cancel();
   this.node.hide();
 };
