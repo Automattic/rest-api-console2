99fffd319b85d688df9c09a29ed1fbf915576de8
Beau Collins <beaucollins@gmail.com>
2014-07-17 13:16:11 -0700

    Using gulp for building the package

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp       = require('gulp'),
+    jade       = require('gulp-jade'),
+    sass       = require('gulp-sass'),
+    concat     = require('gulp-concat'),
+    rename     = require('gulp-rename'),
+    browserify = require('browserify'),
+    source     = require('vinyl-source-stream'),
+    package    = require('./package.json'),
+    exec       = require('child_process').exec,
+    build      = 'dev';
+
+try {
+  var config = require('./config.json');
+} catch (variable) {
+  console.error("Missing config.json, read README.md for instructions.");
+  var config = {};
+}
+
+config.version = package.version;
+
+var public = 'build/wpcom-console/public';
+
+gulp.task("default", ["public", "html", "js", "css"], function() {
+
+});
+
+gulp.task("public", function() {
+  gulp.src('./public/**')
+    .pipe(gulp.dest(public));
+});
+
+gulp.task("config", function(cb) {
+  exec('git describe --always --tag --long --dirty', function(err, stdout, stderr) {
+    config.build = stdout.trim();
+    cb(err);
+  });
+});
+
+gulp.task("html", ["config"], function() {
+  gulp.src('./templates/views/app.jade')
+    .pipe(jade({locals:config}))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest(public));
+});
+
+gulp.task("js", function() {
+  return browserify('./lib/app.js')
+    .transform({global:true}, "uglifyify")
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest(public));
+});
+
+gulp.task("css", function() {
+  gulp.src('templates/sass/*.scss')
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest(public));
+});
