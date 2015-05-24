5a4cefec0f2f66c63210d60de96d9ebb475c3b5a
tellyworth <alex@automattic.com>
2015-05-24 15:01:03 +1000

    Skeleton server and template.

--- rest-api-console.php
+++ rest-api-console.php
@@ -10,3 +10,39 @@
  * Text Domain: rest-api-console
  * Domain Path: /language
  */
+
+class WP_API_Console {
+
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'admin_init' ) );
+		add_action( 'load-tools_page_rest_api_console', array( $this, 'render' ) );
+	}
+	
+	public function admin_init() {
+		$hook = add_management_page( 'Rest API Console', 'Rest API Console', 'manage_options', 'rest_api_console', array( $this, 'nope' ) );
+	}
+	
+	public function nope() {
+	}
+	
+	public function render() {
+		include( __DIR__ . '/templates/views/app.php' );
+		exit;
+	}
+	
+	
+	static function instance() {
+		static $instance = null;
+		
+		if ( is_null( $instance ) ) {
+			$instance = new WP_API_Console();
+		}
+		
+		return $instance;
+	}
+}
+
+
+if ( is_admin() ) {
+	WP_API_Console::instance();
+}
